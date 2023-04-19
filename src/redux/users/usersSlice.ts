9a7239347daca8c462313d38265f3056b3401d78
EmrahMorgil <morgilemrah@gmail.com>
2023-04-19 16:56:43 +0300

    login ekranı bitti type ekldim slice ekranında axios isteği

--- src/redux/users/usersSlice.ts
+++ src/redux/users/usersSlice.ts
@@ -1,17 +1,32 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getUsersAsync: any = createAsyncThunk("users/getUsersAsync",async () => {
+    const res = await axios.get("http://localhost:3004/users");
+    return res.data;
+  }
+);
 
 const initialState: any = {
-//   users: [],
-//   userLoggedIn: false,
-//   registerControl: false,
-//   welcomeUser: "",
+  users: [],
+  userLoggedIn: false,
 };
 
 export const usersSlice = createSlice({
   name: "users",
   initialState,
-  reducers: {},
-  extraReducers: (builder) => {},
+  reducers: {
+    setUserLoggedIn: (state, action) => {
+      state.userLoggedIn = action.payload;
+    }
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getUsersAsync.fulfilled, (state, action) => {
+      console.log(action.payload);
+      state.users = action.payload;
+    });
+  },
 });
 
 export default usersSlice.reducer;
+export const { setUserLoggedIn } = usersSlice.actions;
--- src/types/Type.ts
+++ src/types/Type.ts
@@ -1,3 +1,58 @@
-export class user{
-    
+export class project{
+    id?: number;
+    projectName?: string;
+    createdDate?: string;
+    updatedDate?: string;
+    createdPerson?: string;
+    updatedPerson?: string;
+    contents?: content;
+    totalContent?: number;
+    visibilityRole?: string;
 }
+
+export class content{
+    id?: number;
+    contentName?: string;
+    createdDate?: string;
+    updatedDate?: string;
+    createdPerson?: string;
+    updatedPerson?: string;
+    version?: number;
+    contents?: string;
+    contentTags?: [];
+}
+
+
+export class user{
+    id: number;
+    name: string;
+    surname: string;
+    password: string;
+    role: userRole;
+    visibilityProjects: [];
+    createdDate: string;
+    updatedDate: string;
+    createdPerson: string;
+    updatedPerson: string;
+
+    constructor(id: number, name: string, surname: string, password: string, role: userRole, visibilityProjects: [],createdDate: string, updatedDate: string, createdPerson: string, updatedPerson: string)
+    {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.password = password;
+        this.role = role;
+        this.visibilityProjects = visibilityProjects;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+        this.createdPerson = createdPerson;
+        this.updatedPerson = updatedPerson;
+    }
+
+}
+
+enum userRole{
+    user = 0,
+    admin = 1,
+}
+
