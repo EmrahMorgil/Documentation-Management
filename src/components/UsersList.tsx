9d0d0616dba1bc1b72acaa531effb7f2078c70d4
Ayşe Nur Elaldı <20194703062@ogr.akdeniz.edu.tr>
2023-04-20 11:30:34 +0300

    users listenlenmesi

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,18 +1,44 @@
-import React from 'react'
-import { user } from '../types/Type'
+import React from "react";
+import { user } from "../types/Type";
 
-const UsersList = ({item}: {item: user}) => {
+const UsersList = ({ item }: { item: user }) => {
   return (
-    <div>
-        <div style={{border: "1px solid black"}}>
-        <p>Kullanıcı Adı : {item.name}</p>
-        <p>{item.surname}</p>
-        <p>{item.password}</p>
-        <p>{item.createdPerson}</p>
-        </div>
-        
+    <div className="container mt-5">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name </th>
+            <th>Surname</th>
+            <th>Password </th>
+            <th>Role</th>
+            <th>Projects</th>
+            <th>Created Date</th>
+            <th>Updated Date</th>
+            <th>Created Person</th>
+            <th>Updated Person</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <td>{item.id}</td>
+          <td>{item.name}</td>
+          <td>{item.surname}</td>
+          <td>{item.password}</td>
+          <td>{item.role}</td>
+          <td>{item.visibilityProjects}</td>
+          <td>{item.createdDate}</td>
+          <td>{item.updatedDate}</td>
+          <td>{item.createdPerson}</td>
+          <td>{item.updatedPerson}</td>
+          <td>
+            <button>Update</button>
+            <button>Delete</button>
+          </td>
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default UsersList
+export default UsersList;
