c9a4bdc4f6d1ee0e7f00462c203eb0ee13428a59
Ayşe Nur Elaldı <20194703062@ogr.akdeniz.edu.tr>
2023-05-05 15:54:13 +0300

    name ve total content için sorting eklendi

--- src/components/ProjectComponents/ProjectsList.tsx
+++ src/components/ProjectComponents/ProjectsList.tsx
@@ -1,39 +1,75 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import { project } from "../../types/Type";
 import Project from "./Project";
+import { setProjects } from "../../redux/projects/projectsSlice";
 
-interface IProjectsList{
-  projectsControl: string;
-  userId?: string;
-  filterValue: string;
-}
 
-const ProjectsList: React.FC<IProjectsList> = ({projectsControl, userId, filterValue}) => {
+const ProjectsList = ({projectsControl, userId, filterValue}: {projectsControl: string, userId?: string, filterValue: string}) => {
   const projects = useSelector((state: RootState) => state.projects.projects);
+  // const [data, setData] = useState(projects);
+  const [sorted, setSorted] = useState({ sorted: "totalContent", isReversed: false });
+  const dispatch = useDispatch();
+  
+  const sortById = () => {
+    const sortedData = [...projects].sort((a, b) => {
+      if (sorted.isReversed) {
+        return a.totalContent - b.totalContent;
+      }
+      return b.totalContent - a.totalContent;
+    });
+  
+    dispatch(setProjects(sortedData));
+    console.log(sortedData);
+    setSorted({ sorted: "totalContent", isReversed: !sorted.isReversed });
+  };
+  
+  const sortByProjectName = () => {
+    const sortedData = [...projects].sort((a, b) => {
+      if (sorted.isReversed) {
+        return b.projectName.localeCompare(a.projectName);
+      }
+      return a.projectName.localeCompare(b.projectName);
+    });
+  
+    dispatch(setProjects(sortedData));
+    console.log(sortedData);
+    
+    setSorted({ sorted: "projectName", isReversed: !sorted.isReversed });
+  };
+  
+ const renderArrow=()=>{
+  if (sorted.isReversed) {
+    return  "▲" 
+  }return "▼"
+ }
+
 
   return (
-    <div className="container">
+    <div className="container mt-5">
       <table className="table table-striped">
         <thead className="thead-dark">
           <tr>
             <th scope="col">ID</th>
-            <th scope="col">Project Name </th>
+            <th onClick={sortByProjectName} scope="col">Project Name 
+            {sorted.sorted==="projectName" ? renderArrow():null}  </th>
             <th scope="col">Created Date</th>
             <th scope="col">Updated Date</th>
             <th scope="col">Created Person</th>
             <th scope="col">Updated Person</th>
-            <th scope="col">Total Content</th>
+            <th onClick={sortById} scope="col">Total Content
+            {sorted.sorted==="totalContent" ? renderArrow():null}
+            </th>
             <th scope="col">Visibility Role</th>
-            {projectsControl!="adminLoggedInProjects" && <th scope="col">Actions</th>}
+            {projectsControl!=="homePageProject" && <th scope="col">Actions</th>}
           </tr>
         </thead>
         <tbody>
-          {projects.map((project: project, i: number) =>{
-            if(project.projectName.toLowerCase().includes(filterValue.toLowerCase()))
+          {projects.map((item: project, i: number) =>{
+            if(item.projectName.toLowerCase().includes(filterValue.toLowerCase()))
             {
-              return <Project project={project} key={i} projectsControl={projectsControl} userId={userId}/>
+              return <Project project={item} key={i} projectsControl={projectsControl} userId={userId}/>
             }
           }
           )}
