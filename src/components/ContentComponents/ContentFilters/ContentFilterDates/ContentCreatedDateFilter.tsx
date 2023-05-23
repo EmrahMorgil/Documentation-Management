f765509f727d6e9f03394da69c25ec3d9bfef0dd
EmrahMorgil <morgilemrah@gmail.com>
2023-05-23 14:33:53 +0300

    content date filterlar aynanda kullanınca oluşan bug çözüldü

--- src/components/ContentComponents/ContentFilters/ContentFilterDates/ContentCreatedDateFilter.tsx
+++ src/components/ContentComponents/ContentFilters/ContentFilterDates/ContentCreatedDateFilter.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { DateRangePicker } from 'rsuite'
 import { setContents} from '../../../../redux/contents/contentsSlice';
@@ -7,20 +7,23 @@ import { RootState } from '../../../../redux/store';
 
 const ContentCreatedDateFilter: React.FC = () => {
 
-    const allContents = useSelector((state: RootState)=>state.contents.allContents);
+    const contents = useSelector((state: RootState)=>state.contents.contents);
     const dispatch = useDispatch();
+    const [createdDateFilter, setCreatedDateFilter] = useState();
+
 
     const createdDateRangeFilter = (value: any) => {
         let startDate = new Date(value ? value[0] : "");
         let endDate = new Date(value ? value[1] : "");
         
-        let filteredDates = allContents.filter(function(date: mdlContent) {
+        let filteredDates = contents.filter(function(date: mdlContent) {
           return new Date(date.createdDate) >= startDate && new Date(date.createdDate) <= endDate;
         });
+        setCreatedDateFilter(contents);
         dispatch(setContents(filteredDates));
       };
   return (
-    <DateRangePicker onOk={createdDateRangeFilter} onClean={()=>dispatch(setContents(allContents))} size="sm"/>
+    <DateRangePicker onOk={createdDateRangeFilter} onClean={()=>dispatch(setContents(createdDateFilter))} size="sm"/>
   )
 }
 
--- src/components/ContentComponents/ContentFilters/ContentFilterDates/ContentUpdatedDateFilter.tsx
+++ src/components/ContentComponents/ContentFilters/ContentFilterDates/ContentUpdatedDateFilter.tsx
@@ -1,28 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { DateRangePicker } from 'rsuite'
 import { setContents} from '../../../../redux/contents/contentsSlice';
 import { mdlContent } from '../../../../types/Type';
 import { RootState } from '../../../../redux/store';
 
-const ContentUpdatedDateFilter = () => {
-    const allContents = useSelector((state: RootState)=>state.contents.allContents);
-    const dispatch = useDispatch();
+const ContentUpdatedDateFilter: React.FC = () => {
+
+  const contents = useSelector((state: RootState)=>state.contents.contents);
+  const dispatch = useDispatch();
+  const [updatedDateFilter, setUpdatedDateFilter] = useState();
 
     const updatedDateRangeFilter = (value: any) => {
 
         let startDate = new Date(value ? value[0] : "");
         let endDate = new Date(value ? value[1] : "");
     
-        let filteredDates = allContents.filter(function(date: mdlContent) {
+        let filteredDates = contents.filter(function(date: mdlContent) {
           return new Date(date.updatedDate) >= startDate && new Date(date.updatedDate) <= endDate;
         });
+        setUpdatedDateFilter(contents);
         dispatch(setContents(filteredDates));
       };
     
     
   return (
-    <DateRangePicker onOk={updatedDateRangeFilter} onClean={()=>dispatch(setContents(allContents))} size="sm"/>
+    <DateRangePicker onOk={updatedDateRangeFilter} onClean={()=>dispatch(setContents(updatedDateFilter))} size="sm"/>
   )
 }
 
